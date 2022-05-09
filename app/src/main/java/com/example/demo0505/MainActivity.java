f0a18a85278590f2691693c02789b55c61b96ade
ashleykuan <chiayikuan20@gmail.com>
2022-05-09 19:41:39 +0800

    change to new layout at MainActivity

--- app/src/main/java/com/example/demo0505/MainActivity.java
+++ app/src/main/java/com/example/demo0505/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.google.firebase.database.DatabaseReference;
@@ -12,7 +13,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView iv_employee, iv_employer;
+    Button btn_boss, btn_employer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,19 +28,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void initViews(){
-        iv_employee = findViewById(R.id.employee);
-        iv_employer = findViewById(R.id.employer);
+        btn_boss = findViewById(R.id.btn_boss);
+        btn_employer = findViewById(R.id.btn_employee);
     }
 
     public void setListener() {
-        iv_employer.setOnClickListener(nextPage);
-        iv_employee.setOnClickListener(nextPage);
+        btn_employer.setOnClickListener(nextPage);
+        btn_boss.setOnClickListener(nextPage);
     }
 
     public View.OnClickListener nextPage = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            if(view.getId()==R.id.employee || view.getId()==R.id.employer){
+            if(view.getId()==R.id.btn_employee || view.getId() == R.id.btn_boss){
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
