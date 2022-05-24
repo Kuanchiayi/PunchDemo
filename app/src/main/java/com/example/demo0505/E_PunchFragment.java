649c7be514cd4445be16158379e8fd8cf3f679a2
ashleykuan <chiayikuan20@gmail.com>
2022-05-24 10:39:48 +0800

    change: E_punch\bulletin\salary style login style

--- app/src/main/java/com/example/demo0505/E_PunchFragment.java
+++ app/src/main/java/com/example/demo0505/E_PunchFragment.java
@@ -2,15 +2,23 @@ package com.example.demo0505;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.net.CacheRequest;
 
 public class E_PunchFragment extends Fragment {
 
     View view;
+    TextView time_now;
+    Button btn_punch;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +29,19 @@ public class E_PunchFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_e__punch, container, false);
+        initViews();
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
+    private void initViews(){
+        time_now = view.findViewById(R.id.tv_now);
+        btn_punch = view.findViewById(R.id.btn_punch);
     }
 }
