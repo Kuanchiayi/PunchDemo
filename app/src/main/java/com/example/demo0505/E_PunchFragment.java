44262ec3ded01f64f2050f967e210ed30477b995
ashleykuan <chiayikuan20@gmail.com>
2022-05-25 10:04:36 +0800

    change: E_punch recyclerview

--- app/src/main/java/com/example/demo0505/E_PunchFragment.java
+++ app/src/main/java/com/example/demo0505/E_PunchFragment.java
@@ -1,10 +1,13 @@
 package com.example.demo0505;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.text.format.DateFormat;
@@ -25,7 +28,11 @@ public class E_PunchFragment extends Fragment {
     View view;
     TextView time_now;
     Button btn_punch;
-    RecyclerView recyclerView_punch;
+    RecyclerView recyclerView;
+    Adapter adapter;
+
+    ArrayList<HashMap<String,String>> arrayList = new ArrayList<>();
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,6 +51,8 @@ public class E_PunchFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        /*改成跳動時間*/
+        /*加入資料庫*/
         Calendar cal = Calendar.getInstance();
         CharSequence s = DateFormat.format("yyyy-MM-dd kk:mm:ss", cal.getTime());
         time_now.setText(s);
@@ -53,25 +62,54 @@ public class E_PunchFragment extends Fragment {
             public void onClick(View view) {
                 if (btn_punch.getText().equals("上班打卡")) {
                     btn_punch.setText("下班打卡");
+                    /*加入上班*/
+                    Calendar cal = Calendar.getInstance();
+                    CharSequence on_time = DateFormat.format("yyyy-MM-dd kk:mm:ss", cal.getTime());
+                    HashMap<String, String> data = new HashMap<>();
+//                    data.put("id", title);
+                    data.put("work", "上班");
+                    data.put("time", String.valueOf(on_time));
+                    arrayList.add(data);
                 } else if (btn_punch.getText().equals("下班打卡")) {
                     btn_punch.setText("上班打卡");
+                    Calendar cal = Calendar.getInstance();
+                    CharSequence off_time = DateFormat.format("yyyy-MM-dd kk:mm:ss", cal.getTime());
+                    HashMap<String, String> data = new HashMap<>();
+//                    data.put("id", title);
+                    data.put("work", "下班");
+                    data.put("time", String.valueOf(off_time));
+                    arrayList.add(data);
                 }
+                adapter.notifyDataSetChanged();
             }
         });
     }
     private void initViews(){
         time_now = view.findViewById(R.id.tv_now);
         btn_punch = view.findViewById(R.id.btn_punch);
-        recyclerView_punch = view.findViewById(R.id.recyclerView_punch);
+        recyclerView = view.findViewById(R.id.recyclerView_punch);
+        recyclerView = view.findViewById(R.id.recyclerView_punch);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL));
+        adapter = new Adapter(getActivity(), arrayList);
+        recyclerView.setAdapter(adapter);
     }
     public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder>{
 
+        private Context context;
         public ArrayList<HashMap<String,String>> arrayList;
+        public Adapter(Context context, ArrayList<HashMap<String,String>> arrayList) {
+            this.context = context;
+            this.arrayList = arrayList;
+        }
 
         class ViewHolder extends RecyclerView.ViewHolder{
+            TextView tv_id, tv_time, tv_work;
             public ViewHolder(View itemsView){
                 super(itemsView);
                 tv_id = itemsView.findViewById(R.id.tv_id);
+                tv_work = itemsView.findViewById(R.id.tv_work);
+                tv_time = itemsView.findViewById(R.id.tv_time);
             }
         }
         @NonNull
@@ -82,9 +120,11 @@ public class E_PunchFragment extends Fragment {
         }
 
         @Override
-        public void onBindViewHolder(@NonNull FavAdapter.ViewHolder holder, int position) {
+        public void onBindViewHolder(@NonNull Adapter.ViewHolder holder, int position) {
             HashMap<String,String> data = arrayList.get(position);
-            holder.tv_forumName.setText(data.get("forumName"));
+//            holder.tv_id.setText(data.get("forumName"));
+              holder.tv_work.setText(data.get("work"));
+              holder.tv_time.setText(data.get("time"));
         }
 
         @Override
